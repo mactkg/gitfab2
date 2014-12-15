f673616651bcee27014fe82130bec7639ec10ddb
oshimaryo <oshimaryo@gmail.com>
2014-12-15 08:43:57 +0000

    close #932 fix tag cloud

--- app/controllers/global_projects_controller.rb
+++ app/controllers/global_projects_controller.rb
@@ -29,5 +29,50 @@ class GlobalProjectsController < ApplicationController
       end
       @is_searching = false
     end
+
+
+    @_projects = Project.all().in(is_private: [false, nil])
+    @selected_tags = []
+    file_path = "#{Rails.root}/config/selected-tags.yml"
+
+    tags_list = YAML.load_file file_path
+    file = File::stat file_path
+
+    if tags_list.present? && file.atime - file.mtime < 60 * 60
+      tags_list.each do |tag_name|
+        @selected_tags.push tag_name
+      end
+    else
+      tag_counters = {}
+      @_projects.each do |project|
+        project.tags.each do |tag|
+          if tag_counters.keys.include? tag.name
+            tag_counters[tag.name] += 1
+          else
+            tag_counters.store tag.name, 1
+          end
+        end
+      end
+
+      File.open(file_path, "w"){|file| file = nil}
+      yml_file = File.open file_path, "w"
+
+      if tag_counters.length > 15
+        used_tags = Hash[tag_counters.sort{|(k1, v1), (k2, v2)| v2 <=> v1 }]
+        used_tags.keys.slice(0, 15).each do |tag_name|
+          @selected_tags.push tag_name
+          yml_file.puts "- #{tag_name}"
+        end
+      else
+        tag_counters.keys.each do |tag_name|
+          @selected_tags.push tag_name
+          yml_file.puts "- #{tag_name}"
+        end
+      end
+
+      yml_file.close
+
+    end
+
   end
 end
